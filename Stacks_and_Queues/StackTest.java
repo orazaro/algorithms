422bbea49aef279e02278ff2b021350dcc96d81e
Oleg Razgulyaev <oraz@mail.ru>
2013-04-18 21:02:31 +0400

    generic linked stack

--- Stacks_and_Queues/StackTest.java
+++ Stacks_and_Queues/StackTest.java
@@ -2,7 +2,8 @@ class StackTest {
     public static void main(String[] args) {
         //StackOfStrings stack = new LinkedStackOfStrings();
         //StackOfStrings stack = new FixedCapacityStackOfStrings(100);
-        StackOfStrings stack = new ResizingArrayStackOfStrings();
+        //StackOfStrings stack = new ResizingArrayStackOfStrings();
+        LinkedStack stack = new LinkedStack<String>();
         while(!StdIn.isEmpty()) {
             String s = StdIn.readString();
             if(s.equals("-")) StdOut.println(stack.pop());

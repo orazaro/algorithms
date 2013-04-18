5eca93954d4bd3631d32bc2597853563056459df
Oleg Razgulyaev <oraz@mail.ru>
2013-04-18 21:17:24 +0400

    generic resizable stack

---
+++ Stacks_and_Queues/Stack.java
@@ -0,0 +1,32 @@
+public class Stack<Item> {
+    private Item[] s;
+    private int N = 0;
+
+    public Stack() {
+        s = (Item[]) new Object[1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(Item item) {
+        if(N == s.length) resize(2 * s.length);
+        s[N++] = item;
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for(int i = 0; i < N; i++)
+            copy[i] = s[i];
+        s = copy;
+    }
+
+    public Item pop() {
+        Item item = s[--N];
+        s[N] = null;    // to free unused memory
+        if(N > 0 && N == s.length/4)    // if 1/4 of capacity
+            resize(s.length/2);         // shrink to 1/2 of capacity
+        return item;
+    }
+}
--- Stacks_and_Queues/StackTest.java
+++ Stacks_and_Queues/StackTest.java
@@ -3,7 +3,8 @@ class StackTest {
         //StackOfStrings stack = new LinkedStackOfStrings();
         //StackOfStrings stack = new FixedCapacityStackOfStrings(100);
         //StackOfStrings stack = new ResizingArrayStackOfStrings();
-        LinkedStack stack = new LinkedStack<String>();
+        //LinkedStack stack = new LinkedStack<String>();
+        Stack stack = new Stack<String>();
         while(!StdIn.isEmpty()) {
             String s = StdIn.readString();
             if(s.equals("-")) StdOut.println(stack.pop());

065cbf9e155d45db78478c5224b2699b9c72dcd7
Oleg Razgulyaev <oraz@mail.ru>
2013-04-29 22:12:42 +0400

    Symbol_Tables

---
+++ Symbol_Tables/FrequencyCounter.java
@@ -0,0 +1,22 @@
+
+public class FrequencyCounter
+{
+    public static void main(String[] args)
+    {
+        int minlen = Integer.parseInt(args[0]);
+        ST<String, Integer> st = new ST<String, Integer>();
+        while (!StdIn.isEmpty())
+        {
+            String word = StdIn.readString();
+            if (word.length() < minlen) continue;   // ignore short words
+            if (!st.contains(word)) st.put(word, 1);
+            else                    st.put(word, st.get(word) + 1);
+        }
+        String max = "";
+        st.put(max, 0);
+        for (String word : st.keys())
+            if (st.get(word) > st.get(max))
+                max = word;
+        StdOut.println(max + " " + st.get(max));
+    }
+}

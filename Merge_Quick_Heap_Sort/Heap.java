91b1ac2e944cb4a2bbd56c23f6cbf0968573a77f
Oleg Razgulyaev <oraz@mail.ru>
2013-04-29 12:53:48 +0400

    fix heapsort

--- Merge_Quick_Heap_Sort/Heap.java
+++ Merge_Quick_Heap_Sort/Heap.java
@@ -4,9 +4,13 @@ public class Heap
     {
         int N = a.length;
         int N0 = N;
+
+        /* from unordered array to heap */
         for (int k = N/2; k >= 1; k--)
             sink(a, k, N);
-        while (N > 0)
+        
+        /* remove sequentally elements from heap top */
+        while (N > 1)
         {
             exch(a, 1, N);
             sink(a, 1, --N);

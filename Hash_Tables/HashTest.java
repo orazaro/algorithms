5c7ba7b31c6a0b49bbe215aeaa4d28958c6ddf9e
Oleg Razgulyaev <oraz@mail.ru>
2013-05-06 16:08:26 +0400

    Horner method

--- Hash_Tables/HashTest.java
+++ Hash_Tables/HashTest.java
@@ -1,10 +1,21 @@
 
 public class HashTest 
 {
+    public static int Horner(String s)
+    {
+        int hash = 0;
+        for (int i = 0; i < s.length(); i++)
+            hash = s.charAt(i) + (31 * hash);
+        return hash;
+    }
+
     public static void main(String[] args)
     {
         Boolean b = true;   StdOut.println(b.hashCode());
         b = false;          StdOut.println(b.hashCode());
         Integer i = 12345;  StdOut.println(i.hashCode());
+    
+        String s = "TestHornerMethod";
+        StdOut.printf("%d %d\n", s.hashCode(), Horner(s));
     }
 }

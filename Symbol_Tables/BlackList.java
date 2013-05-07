da2e77249aa08a5bfa2418d63dc4a0633ecda4c2
Oleg Razgulyaev <oraz@mail.ru>
2013-05-07 10:39:07 +0400

    BlackList

---
+++ Symbol_Tables/BlackList.java
@@ -0,0 +1,19 @@
+
+public class BlackList
+{
+    public static void main(String[] args)
+    {
+        SET<String> set = new SET<String>();
+
+        In in = new In(args[0]);
+        while (!in.isEmpty())
+            set.add(in.readString());
+
+        while (!StdIn.isEmpty())
+        {
+            String word = StdIn.readString();
+            if (!set.contains(word))
+                StdOut.println(word);
+        }
+    }
+}

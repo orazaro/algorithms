688b122023fbf26d3deda8142eb5db7ebf848d26
Oleg Razgulyaev <oraz@mail.ru>
2013-05-18 18:05:22 +0400

    Undirected Graphs

---
+++ Undirected_Graphs/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+    public static void main(String[] args) {
+        //In in = new In(args[0]);
+        //Graph G = new Graph(in);
+        Graph G = new Graph(3);
+        G.addEdge(0,1);
+        G.addEdge(0,2);
+        G.addEdge(2,1);
+
+        for (int v = 0; v < G.V(); v++)
+            for (int w : G.adj(v))
+                StdOut.println(v + "-" + w);
+    }
+}
